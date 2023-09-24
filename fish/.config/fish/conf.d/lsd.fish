943b026f8cdf40685d5fe24fa9b14da5fad0824c
Connor Sullivan <dev.connorjs@gmail.com>
2023-09-24 10:43:52 -0400

    🚚 Use conf.d for fish abbreviations

---
+++ fish/.config/fish/conf.d/lsd.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+	abbr -a l   lsd
+	abbr -a la  lsd -a
+	abbr -a ls  lsd
+	abbr -a ll  lsd -l
+	abbr -a lt  lsd --tree -l
+	abbr -a lt2 lsd --tree -l --depth 2
+	abbr -a lt3 lsd --tree -l --depth 3
+	abbr -a ltd lsd --tree -l --depth
+	abbr -a t   lsd --tree
+	abbr -a t2  lsd --tree --depth 2
+	abbr -a t3  lsd --tree --depth 3
+	abbr -a td  lsd --tree --depth
+end
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,21 +1,3 @@
-if status is-interactive
-	# Commands to run in interactive session go here
-
-	# LSD
-	abbr -a l   lsd
-	abbr -a la  lsd -a
-	abbr -a ls  lsd
-	abbr -a ll  lsd -l
-	abbr -a lt  lsd --tree -l
-	abbr -a lt2 lsd --tree -l --depth 2
-	abbr -a lt3 lsd --tree -l --depth 3
-	abbr -a ltd lsd --tree -l --depth
-	abbr -a t   lsd --tree
-	abbr -a t2  lsd --tree --depth 2
-	abbr -a t3  lsd --tree --depth 3
-	abbr -a td  lsd --tree --depth
-end
-
 # CLI
 abbr -a d 'pushd'
 abbr -a cd 'pushd'
